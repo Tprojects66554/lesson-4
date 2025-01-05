cc7f8019df827c78d6baeb5054cbcf0cb6123843
Tprojects66554 <38327606992@mby.co.il>
2025-01-05 15:08:10 +0200

    commit-1

--- 1.js
+++ 1.js
@@ -1,7 +1,14 @@
 //bs"d
 const a=()=>{
 
-    console.log("AAAAAAAAAAAA")
+    console.log("AAAAAAAAAAAA")}
 const c=()=>{
     console.log("ccccccccccc")
 }
+const D=()=>{
+    console.log("dddddddddddddddd")
+    }
+
+
+
+
